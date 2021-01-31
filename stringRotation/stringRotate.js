33fdc5e6fbd40beb7b4158cd8881475057389632
Yuri <yuri_dsx_@hotmail.com>
2021-01-31 15:54:11 -0300

    change to clean if statement

--- stringRotation/stringRotate.js
+++ stringRotation/stringRotate.js
@@ -1,9 +1,7 @@
-const isRotation = (a,b) =>{
-    str = b+b
-    if(a.length === b.length && str.includes(a)){
-        return true
-    }
+const isRotation = (a, b) => {
+    str = b + b
+    if (a.length === b.length && str.includes(a)) return true
     return false
-}   
+}
 
 console.log("É uma rotação: " + isRotation('abcde', 'cdeab'))
