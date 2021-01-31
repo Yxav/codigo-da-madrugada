b48741dfed35b88126491f688540664b14c1941f
Yuri <yuri_dsx_@hotmail.com>
2021-01-31 15:50:44 -0300

    Add challenge rotation string

---
+++ stringRotation/stringRotate.js
@@ -0,0 +1,9 @@
+const isRotation = (a,b) =>{
+    str = b+b
+    if(a.length === b.length && str.includes(a)){
+        return true
+    }
+    return false
+}   
+
+console.log("É uma rotação: " + isRotation('abcde', 'cdeab'))
